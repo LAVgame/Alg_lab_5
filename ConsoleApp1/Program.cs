00a600d38cb36c28b81a8b8b5b04132399a767ca
LAVgame <vipaleksei007@gmail.com>
2023-10-09 18:57:35 +0300

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+var sw = new Stopwatch();
+sw.Start();
+Console.Write("Введи размерность массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int k = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-30,30);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine("Сортированый массив: ");
+int temp = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = i + 1; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    Console.WriteLine(arr[i]);
+}
+sw.Stop();
+Console.WriteLine($"StopWatch:{sw.Elapsed}");
+Console.ReadLine();
+
+
